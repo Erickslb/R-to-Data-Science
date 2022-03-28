3800790535c39179dda2e5bc862ca485c52938b0
erickslb <erick.slbrito@gmail.com>
2022-03-28 12:55:50 -0300

    missao 1 - enunciado

---
+++ lectures/aula 9 .R
@@ -0,0 +1,43 @@
+### Estrutura das colunas
+
+
+# Idade
+# Gênero: 
+# Escolaridade: 
+# Região onde mora: 
+# Renda pessoal bruta:
+# Você procura informações a respeito das ações de empresas que promovam sustentabilidade?
+# Onde você busca informações sobre ações de sustentabilidade das empresas?
+# Você dá preferência a comprar produtos de marcas sustentáveis ?
+# O que você entende como sustentabilidade considerando três pilares para o desenvolvimento sustentável dos países: econômico, social e ambiental
+# " [•	Todo negócio deve ter uma responsabilidade social e ambiental além do objetivo de lucro]"
+# " [•	Minhas ações impactam o meio ambiente.]"
+# " [•	“Estou preocupado com as questões econômicas do país e do mundo”]"
+# " [•	“Sou engajado a causas sociais”]"
+# " [•	“Eu faço o que está ao meu alcance para amenizar as desigualdades sociais”]"
+# " [•	Eu estaria disposto(a) a pagar mais caro por produtos de empresas que promovem e defendem a sustentabilidade]"
+# " [•	Eu estaria tendencioso(a) a comprar marcas sustentáveis em vez de marcas não sustentáveis]"
+# " [•	Eu estaria disposto(a) a pagar mais caro por produtos de empresas que protegem o ambiente]"
+# "Para você, quem são os agentes responsáveis para solucionar os problemas ambientais, econômicos e sociais? 	"
+# "Entre as empresas abaixo, quais que você possui conhecimento que praticam o desenvolvimento sustentável? 		"
+# Como você tomou conhecimento das ações de desenvolvimento sustentável que essas empresas praticam?  
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Qualidade do Produto]"
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Preço]"
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Localização]"
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Embalagem]"
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Práticas Sustentáveis]"
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Propaganda e influência da mídia]"
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Evidências no Ponto de Venda]"
+# "Entre os fatores abaixo, pontue qual a relevância deles na decisão de compra de uma marca.			 [Atendimento]"
+# Se a empresa não for sustentável, você consome essa marca?
+# "Quando tenho oportunidade, me sinto motivado para mudar para as marcas que ajudam a natureza e a comunidade?			"
+
+### Missão..
+
+# Você e sua equipe (máximo 5 estudantes) devem elaborar um cruzamento de, pelo 
+# menos, 5 variáveis e tentar identificar se algum perfil específico tem um 
+# alguma atitude/ação em prol de alguma das ações descritas. 
+
+### Apresentação quinta. Equipe com maior "desenvoltura" ganhará um plus no 
+# linkedin e uma recomendação... se empatarem dois grupos todos ganham 
+# apenas plus. 
